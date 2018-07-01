00f3a3ed780d6b01ef2bda6632ccfae17ce63acd
seewilds <40343199+seewilds@users.noreply.github.com>
2018-06-30 22:14:16 -0400

    Added chron::julian to eq_clean, ggplot2::aes to timelines.

--- tests/testthat/test_geom_timeline.R
+++ tests/testthat/test_geom_timeline.R
@@ -1,7 +1,7 @@
 context("Test Timeline Stat and Geom")
 
 usa_hurricanes <- final_hurricanes%>%dplyr::filter(COUNTRY == "USA")
-t <- ggplot2::ggplot(data = usa_hurricanes, ggpllot2::aes(DATE, COUNTRY)) + geom_timeline(ggplot2::aes(xmin =as.Date("1999-01-01")))
+t <- ggplot2::ggplot(data = usa_hurricanes, ggplot2::aes(DATE, COUNTRY)) + geom_timeline(ggplot2::aes(xmin =as.Date("1999-01-01")))
 
 test_that("t is a list", {
   expect_true(typeof(t) == "list")
--- tests/testthat/test_geom_timeline_label.R
+++ tests/testthat/test_geom_timeline_label.R
@@ -1,8 +1,8 @@
 context("Test Timeline Label stat and geom")
 
-t <- ggplot2::ggplot(data = usa_hurricanes, aes(DATE, COUNTRY)) 
-t <- t + geom_timeline(aes(xmin =as.Date("1999-01-01")))  
-t <- t + geom_timeline_label(aes(xmin =as.Date("1990-01-01"), label = LOCATION_NAME))
+t <- ggplot2::ggplot(data = usa_hurricanes, ggplot2::aes(DATE, COUNTRY)) 
+t <- t + geom_timeline(ggplot2::aes(xmin =as.Date("1999-01-01")))  
+t <- t + geom_timeline_label(ggplot2::aes(xmin =as.Date("1990-01-01"), label = LOCATION_NAME))
 
 test_that("t is a list", {
   expect_true(typeof(t) == "list")
